8bcb64fc22cc7182d54ea4da09c08dc678927ad6
ademalabas@gmail.com <ademalabas@gmail.com>
2023-01-11 23:18:17 +0300

    11.01.23

--- src/test/java/stepdefinitions/OrangeHRMStepDefinitions.java
+++ src/test/java/stepdefinitions/OrangeHRMStepDefinitions.java
@@ -1,9 +1,37 @@
 package stepdefinitions;
 
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+import utilities.Driver;
+
 public class OrangeHRMStepDefinitions {
 
 
+    @Given("kullanici HRMLogin sayfasina gider")
+    public void kullaniciHRMLoginSayfasinaGider() {
+        //Driver.getDriver().get("https://opensource-demo.orangehrmlive.com/");
 
+    }
+
+    @When("kullanici valid credentials girer")
+    public void kullanicivalidCredentialsGirer(DataTable table) {
+
+     String usernamr  =table.cell(1,0);
+      String password =table.cell(1,1);
+
+
+
+    }
+
+    @When("kullanici invalid credentials girer")
+    public void kullaniciinvalidCredentialsGirer(DataTable table) {
+
+
+
+
+    }
 
 
 }
