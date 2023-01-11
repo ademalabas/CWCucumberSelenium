cda88d1978a1a5ac443610af5b4ac0cbaf3a1156
ademalabas@gmail.com <ademalabas@gmail.com>
2023-01-11 19:42:28 +0300

    11.01.23

--- src/test/java/runners/TagRunner.java
+++ src/test/java/runners/TagRunner.java
@@ -1,4 +1,20 @@
 package runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+// Sadece @smoke tagine sahip olan senaryolari calistiran runner
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features/01_Tags.feature",
+        glue = "stepdefinitions",
+        tags ="@smoke or @regression" ,
+        dryRun = false
+)
+
 public class TagRunner {
+
+
 }
